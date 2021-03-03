2ce772376389d2ec2ea9cbce358e2dd3f29c51c9
sebastian spadea <spadea.sebastian@gmail.com>
2021-03-03 13:52:02 -0300

    Web scraping with puppeteer

---
+++ index.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer')
+
+const webScrapper = async (url, seccion) => {
+  try {
+    const browser= await puppeteer.launch({
+      headless: false
+    });
+    const page=await browser.newPage();
+  
+    let response = await loadUrl(page, url, browser, seccion);
+    return response
+  } catch(err) {
+    console.log(err)
+  }
+};
+
+async function loadUrl(page, url, browser, seccion){
+  await page.goto(url,{
+    awaitUntil:["load","domcontentloaded","networkidle0","networkidle2"]
+  });
+
+  const valorEncontrado=await page.$eval(seccion, el=>el.innerHTML)
+  await console.log(`Valor: ${valorEncontrado}`);
+  browser.close();
+  return valorEncontrado
+};
+
+let blueAmbitoCompra = webScrapper("https://www.ambito.com/contenidos/dolar.html",".variacion-max-min-chico > .d-flex  > .first > .data-compra");
+let blueAmbitoVenta = webScrapper("https://www.ambito.com/contenidos/dolar.html",".variacion-max-min-chico > .d-flex  > .second > .data-venta");
+
+let blueDolarHoyCompra = webScrapper("https://www.dolarhoy.com/",".dolar > .is-parent > .is-child > .values > .compra > .val");
+let blueDolarHoyVenta = webScrapper("https://www.dolarhoy.com/",".dolar > .is-parent > .is-child > .values > .venta > .val");
+
+let blueCronistaVenta = webScrapper("https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB",".markets > #market-scrll-1 > li > a > .sell > .sell-wrapper > .sell-value")
+let blueCronistaCompra = webScrapper("https://www.cronista.com/MercadosOnline/moneda.html?id=ARSB",".markets > #market-scrll-1 > li > a > .buy > .buy-wrapper > .buy-value")
+
+
+
+
+console.log(`Valor dólar blue ámbito: ${blueAmbitoCompra} para la compra y ${blueAmbitoVenta} para la venta`);
+console.log(`Valor dólar blue dolarHoy: ${blueDolarHoyCompra} para la compra y ${blueDolarHoyVenta} para la venta`);
+console.log(`Valor dólar blue cronista: ${blueCronistaCompra} para la compra y ${blueCronistaVenta} para la venta`);
+
